a1f724833dbf6a69ef7935e1fb7db74454cc7d02
abbosteqqed <abbos@teqqed.com>
2025-05-07 17:42:58 +0500

    customer create added to webhook

--- src/app/api/webhook/polar/route.ts
+++ src/app/api/webhook/polar/route.ts
@@ -47,6 +47,25 @@ export async function POST(request: NextRequest) {
 
 	const { type, data } = webhookPayload;
 
+	if (type === "customer.created") {
+		const customer = await db.user.findUnique({
+			where: {
+				email: data.email,
+			},
+		});
+
+		if (customer) {
+			await db.user.update({
+				where: {
+					id: customer.id,
+				},
+				data: {
+					customerId: data.id,
+				},
+			});
+		}
+	}
+
 	if (type === "checkout.created") {
 		await db.checkout.create({
 			data: {
