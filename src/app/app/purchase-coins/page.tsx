0b42e4d89f8ac7d53f0c48ba34d0022e28ab833e
abbosteqqed <abbos@teqqed.com>
2025-05-11 11:27:33 +0500

    coin pruchase back

--- src/app/app/purchase-coins/page.tsx
+++ src/app/app/purchase-coins/page.tsx
@@ -1,11 +1,7 @@
-"use client";
-import dynamic from "next/dynamic";
+
+import CoinPurchase from "@/features/purchase-coins/components/coin-purchase";
 import React from "react";
 
-const CoinPurchase = dynamic(
-	() => import("@/features/purchase-coins/components/coin-purchase"),
-	{ ssr: false }
-);
 
 const PurchaseCoins = () => {
 	return <CoinPurchase />;
