c89cecc1ecd14a5dd7ee5d912f2e7102a0dcf4e3
abbosteqqed <abbos@teqqed.com>
2025-05-15 09:56:51 +0500

    fixed onboarding

---
+++ src/app/onboarding/loading.tsx
@@ -0,0 +1,8 @@
+import LoadingPage from "@/components/layout/loading-page";
+import React from "react";
+
+const Onboarding = () => {
+	return <LoadingPage message="Onboarding..." />;
+};
+
+export default Onboarding;
--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,31 +1,15 @@
-"use client";
-import LoadingPage from "@/components/layout/loading-page";
 import { getCurrentUser } from "@/features/auth/actions/get-curent-user";
 import StepForm from "@/features/onboarding/components/step-form";
-import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { redirect } from "next/navigation";
 
-const OnboardingPage = () => {
-	const router = useRouter();
-	const { data, isPending } = useQuery({
-		queryKey: ["user"],
-		queryFn: getCurrentUser,
-	});
-
-	useEffect(() => {
-		if (data) {
-			if (data.user.onboarding) {
-				router.replace("/onboarding");
-			}
-			if (!data.user.subscription && !data.user.onboarding) {
-				router.replace("/limited-pricing");
-			}
-		}
-	}, [data, router]);
-	if (!isPending) {
-		return <LoadingPage message="Onboarding..." />;
+const OnboardingPage = async() => {
+	const currentUser = await getCurrentUser()
+	if(!currentUser){
+		return redirect("/auth/signin")
+	}
+	if(!currentUser.user.onboarding){
+		return redirect("/app");
 	}
 	return (
 		<div className="relative flex min-h-screen p-6 w-screen items-center justify-center overflow-hidden md:p-0">
