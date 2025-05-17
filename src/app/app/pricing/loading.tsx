17f591665fb72940d288b6d6317e0fb8b8bfca7a
abbosteqqed <abbos@teqqed.com>
2025-05-17 18:24:23 +0500

    loading added

---
+++ src/app/app/pricing/loading.tsx
@@ -0,0 +1,8 @@
+import LoadingPage from "@/components/layout/loading-page";
+import React from "react";
+
+const Loading = () => {
+	return <LoadingPage message="Manage plan.." />;
+};
+
+export default Loading;
